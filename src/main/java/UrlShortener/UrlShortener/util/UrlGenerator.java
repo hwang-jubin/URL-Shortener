4eead81eb5c2b641cbb9bace9effac0183163919
hwang-jubin <117631618+hwang-jubin@users.noreply.github.com>
2023-10-18 14:20:22 +0900

    feat: Url generator
    service에서 만들어진 shortenUrl 을 fullUrl로 만들어주는 generator

---
+++ src/main/java/UrlShortener/UrlShortener/util/UrlGenerator.java
@@ -0,0 +1,18 @@
+package UrlShortener.UrlShortener.util;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UrlGenerator {
+
+    @Value("${server.domain}")
+    private String domain;
+
+    public String generator(String encodedUrl){
+        return "http://"+this.domain+":8080/"+encodedUrl;
+    }
+
+}
