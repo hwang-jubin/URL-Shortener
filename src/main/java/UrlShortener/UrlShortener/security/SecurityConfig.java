c0b9d5f6230b4bd515f08b13f4af04546aaabf88
황주빈 <hjb06288@gmail.com>
2024-01-06 16:37:12 +0900

    feat: spring security 경로 config

--- src/main/java/UrlShortener/UrlShortener/security/SecurityConfig.java
+++ src/main/java/UrlShortener/UrlShortener/security/SecurityConfig.java
@@ -1,7 +1,13 @@
 package UrlShortener.UrlShortener.security;
 
+import UrlShortener.UrlShortener.config.CredentialConfig;
 import UrlShortener.UrlShortener.jwt.JwtAuthenticationFilter;
+import UrlShortener.UrlShortener.jwt.filter.CustomJwtFilter;
+import UrlShortener.UrlShortener.jwt.filter.CustomLoginFilter;
+import UrlShortener.UrlShortener.jwt.provider.CustomJwtProvider;
 import UrlShortener.UrlShortener.jwt.TokenProvider;
+import UrlShortener.UrlShortener.jwt.provider.CustomLoginProvider;
+import UrlShortener.UrlShortener.jwt.token.CustomLoginToken;
 import UrlShortener.UrlShortener.repository.MemberRepository;
 import UrlShortener.UrlShortener.util.ResolveToken;
 import lombok.RequiredArgsConstructor;
@@ -9,18 +15,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFilter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CsrfFilter;
 
 //기본적인 web 보완 활성화 어노테이션
 @EnableWebSecurity
@@ -28,45 +34,76 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @RequiredArgsConstructor
 public class SecurityConfig{
 
-    private final UserDetailsService userDetailsService;
-    private final TokenProvider tokenProvider;
     private final ResolveToken resolveToken;
+    private final CustomJwtProvider customJwtProvider;
+    private final ClientUserDetailService clientUserDetailService;
+    private final CredentialConfig credentialConfig;
 
-    private final MemberRepository memberRepository;
+    //providerManager의 목록에 customJwtProvider 등록
 
-    //passwordEncoder 의 구현체로 BcryptpasswordEncoder를 bean 으로 등록해서 주입
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(new CustomJwtProvider(clientUserDetailService, credentialConfig));
+        authenticationManagerBuilder.authenticationProvider(new CustomLoginProvider(clientUserDetailService, passwordEncoder()));
+        return authenticationManagerBuilder.build();
+    }
+
+    //passwordEncoder 의 구현체로 BcryptpasswordEncoder를 bean 으로 등록
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
 
         return new BCryptPasswordEncoder();
-
     }
+
+
 
     //인증, 인가
     //일부 경로는 token이 없어도 접근 가능하게 함
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http
+
+        http    // CSRF 보안 비활성화
+                .csrf().disable()
                 // 세션 비활성화
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-
-                // 인증이 필요한 api 와 아닌 api 를 구별
-                .authorizeRequests()
-                .antMatchers("/member/signup","/member/login","/shorten/**")
-                .permitAll()
-                .anyRequest().authenticated()
 
                 .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)) // 인증이 필요한 경우 401 상태 코드 반환
 
                 .and()
-                // CSRF 보안 비활성화
-                .csrf().disable()
-                .addFilterBefore(new JwtAuthenticationFilter(tokenProvider, resolveToken), UsernamePasswordAuthenticationFilter.class);
+                .authorizeRequests()
+                .antMatchers("/member/signup").permitAll()
+                .and()
+                .antMatcher("/member/login")
+                .addFilterAt(new CustomLoginFilter(authenticationManager(null)), UsernamePasswordAuthenticationFilter.class);
+
 
         return http.build();
     }
+
+
+//    @Bean
+//    public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception{
+//
+//        http
+//                .csrf().disable()   //csrf 비활성화하고자 하는 경우
+//                // 세션 비활성화
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .antMatcher("/shorten/url")
+//                .authorizeRequests()
+//                .antMatchers("/shorten/url").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .addFilterBefore(new CustomJwtFilter(authenticationManager(null), resolveToken), UsernamePasswordAuthenticationFilter.class)
+//
+//        return http.build();
+//
+//    }
+
 }
