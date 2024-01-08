2f7270f4eab615680ac7b98a8aa462c46718afbc
황주빈 <hjb06288@gmail.com>
2024-01-08 17:38:39 +0900

    feat: 기존 filter 삭제에 따른 AUTHORIZATION_HEADER 정보 credential config 로 이동

--- src/main/java/UrlShortener/UrlShortener/util/ResolveToken.java
+++ src/main/java/UrlShortener/UrlShortener/util/ResolveToken.java
@@ -1,15 +1,15 @@
 package UrlShortener.UrlShortener.util;
 
+import UrlShortener.UrlShortener.config.CredentialConfig;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
-import static UrlShortener.UrlShortener.jwt.JwtAuthenticationFilter.AUTHORIZATION_HEADER;
 
 @Component
 public class ResolveToken {
     public String resolveToken(HttpServletRequest request){
-        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        String bearerToken = request.getHeader(CredentialConfig.AUTHORIZATION_HEADER);
 
         if(bearerToken != null && bearerToken.startsWith("Bearer ")){
             return bearerToken.substring(7);
