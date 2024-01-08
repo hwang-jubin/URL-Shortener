95de387b6b07512c176baf1bb05039691cc6a548
황주빈 <hjb06288@gmail.com>
2024-01-08 17:52:34 +0900

    feat: login filter를 거친 후 token발급 로직 추가

--- src/main/java/UrlShortener/UrlShortener/service/AuthenticationService.java
+++ src/main/java/UrlShortener/UrlShortener/service/AuthenticationService.java
@@ -1,20 +1,16 @@
 package UrlShortener.UrlShortener.service;
 
-import UrlShortener.UrlShortener.domain.Member;
-import UrlShortener.UrlShortener.jwt.JwtAuthenticationFilter;
-import UrlShortener.UrlShortener.jwt.TokenProvider;
+import UrlShortener.UrlShortener.config.CredentialConfig;
+import UrlShortener.UrlShortener.jwt.TokenGenerator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 @Slf4j
@@ -22,30 +18,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class AuthenticationService {
 
-    private final UserDetailsService userDetailsService;
-    private final AuthenticationManagerBuilder authenticationManager;
-    private final TokenProvider tokenProvider;
+    private final TokenGenerator tokenGenerator;
 
-    public ResponseEntity login(Member member){
+    public ResponseEntity login(){
 
-        //1. AuthenticationManager를 통해 인증을 시도하고 인증이 성공하면 Authentication 객체를 리턴받는다.
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(member.getLoginId(), member.getPassword());
-
-        // 해당 부분에 왔을 때 UserDetailsServiceImpl의 loadUserByUsername() 메소드가 실행된다.
-        Authentication authentication = authenticationManager.getObject().authenticate(authenticationToken);
-
-        // authentication 객체를 createToken 메소드를 통해서 JWT Token을 생성
-        String jwt = tokenProvider.generateToken(authentication.getName());
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        //이름? id?
+        String jwt = tokenGenerator.generateToken(principal.toString());
 
         HttpHeaders httpHeaders = new HttpHeaders();
-
         // response header에 jwt token에 넣어줌
-        httpHeaders.add(JwtAuthenticationFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+        httpHeaders.add(CredentialConfig.AUTHORIZATION_HEADER, "Bearer " + jwt);
 
         return new ResponseEntity<>(httpHeaders, HttpStatus.OK);
     }
-
-
-
 }
