d4855f40a693cfe2a00e8f408c85165cd82278dc
황주빈 <hjb06288@gmail.com>
2024-01-10 15:05:39 +0900

    fix: url 삭제 api의 return 객체를 새로 생성한 Dto로 변경

--- src/main/java/UrlShortener/UrlShortener/responseDto/ShortenUrlDeleteDto.java
+++ src/main/java/UrlShortener/UrlShortener/responseDto/ShortenUrlDeleteDto.java
@@ -1,36 +1,32 @@
 package UrlShortener.UrlShortener.responseDto;
 
-
 import UrlShortener.UrlShortener.domain.ShortenUrl;
 import UrlShortener.UrlShortener.util.UrlGenerator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
-public class ShortenUrlDto {
+public class ShortenUrlDeleteDto {
 
     @JsonIgnore
     private final UrlGenerator urlGenerator;
-    
+
     private Long shortenUrlId;
     private String originUrl;
     private String shortenUrl;
     private Long memberId;
     private LocalDateTime createdShortenUrlDate;
-    private LocalDateTime deletedShortenUrlDate;
+    private LocalDateTime deleteShortenUrlDate;
 
-
-    public ShortenUrlDto(UrlGenerator urlGenerator, ShortenUrl shortenUrl) {
+    public ShortenUrlDeleteDto(UrlGenerator urlGenerator, ShortenUrl shortenUrl) {
         this.urlGenerator = urlGenerator;
         this.shortenUrlId = shortenUrl.getId();
-       this.originUrl = shortenUrl.getOriginUrl();
-       this.shortenUrl = this.urlGenerator.generator(shortenUrl.getShortenUrl());
-       this.memberId = shortenUrl.getMember().getId();
-       this.createdShortenUrlDate = shortenUrl.getCreateShortenUrlDate();
-       this.deletedShortenUrlDate =shortenUrl.getDeleteShortenUrlDate();
+        this.originUrl = shortenUrl.getOriginUrl();
+        this.shortenUrl = this.urlGenerator.generator(shortenUrl.getShortenUrl());
+        this.memberId = shortenUrl.getMember().getId();
+        this.createdShortenUrlDate = shortenUrl.getCreateShortenUrlDate();
+        this.deleteShortenUrlDate = shortenUrl.getDeleteShortenUrlDate();
     }
-
 }
